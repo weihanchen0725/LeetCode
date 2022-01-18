09f0f9dd95e33c1843b7e032a26f3df0c9699911
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-01-18 13:09:10 -0800

    Time: 11 ms (37.06%), Space: 51.6 MB (37.97%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean validPalindrome(String s) {
         if(s == null) return false;
         if(s.length() == 0) return true;
-        int length = s.length(), left = 0, right = length-1;
+        int left = 0, right = s.length()-1;
         while(left < right){
             if(s.charAt(left) == s.charAt(right)){
                 left++;
