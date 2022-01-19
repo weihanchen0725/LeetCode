c59f3265ae90da46575abbbe1e347cff63f7b651
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-01-19 10:58:44 -0800

    Time: 2 ms (56.23%), Space: 57 MB (27.77%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] results = new int[length];
+        results[0] = 1;
+        for(int index = 1; index < length; index++){
+            results[index] = results[index-1] * nums[index-1];
+        }
+        int temp = 1;
+        for(int index = length - 2; index >= 0; index--){
+            results[index] *= temp * nums[index+1];
+            temp *= nums[index+1];
+        }
+        return results;
+    }
+}
