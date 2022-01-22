ad879eca296fd5ae0e54384d3638d959a2cf351c
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-01-21 16:58:47 -0800

    Time: 4 ms (97.31%), Space: 38.8 MB (89.79%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -4,10 +4,7 @@ class Solution {
         int start = 0, maxLength = 0, maxCount = 0;
         for(int end = 0; end < s.length(); end++){
             maxCount = Math.max(maxCount, ++count[s.charAt(end)-'A']);
-            while(end-start-maxCount+1>k){
-                count[s.charAt(start)-'A']--;
-                start++;
-            }
+            while(end-start-maxCount+1>k) count[s.charAt(start++)-'A']--;
             maxLength = Math.max(maxLength, end-start+1);
         }
         return maxLength;
