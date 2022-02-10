0d47eac3f4cb9f6b827ab1ac9e160771569e20dc
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-09 18:43:22 -0800

    Time: 1 ms (94.69%), Space: 42.5 MB (10.73%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -5,8 +5,9 @@ public class Solution {
         for(int index = 0; index < 32; index++){
             result += n & 1;
             n >>>= 1;
-            if(index < 31)
+            if(index < 31){
                 result <<= 1;
+            }
         }
         return result;
     }
