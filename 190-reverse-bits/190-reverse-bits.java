5d998f878ca15fccd03926e03ec86f7bc5d96b42
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-09 18:42:23 -0800

    Time: 1 ms (94.69%), Space: 43.1 MB (5.65%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.java
@@ -0,0 +1,13 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        for(int index = 0; index < 32; index++){
+            result += n & 1;
+            n >>>= 1;
+            if(index < 31)
+                result <<= 1;
+        }
+        return result;
+    }
+}
