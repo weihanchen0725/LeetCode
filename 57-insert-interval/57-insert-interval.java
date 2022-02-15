c8b1543f67faadde7b17e1ed431bdcbfa2794f00
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-15 12:53:32 -0800

    Time: 2 ms (72.15%), Space: 47.8 MB (12.37%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -2,8 +2,8 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int newStart = newInterval[0], newEnd = newInterval[1];
         ArrayList<int[]> list = new ArrayList<>();
-        for(int[] curIntervals : intervals){
-            int curStart = curIntervals[0], curEnd = curIntervals[1];
+        for(int[] curInterval : intervals){
+            int curStart = curInterval[0], curEnd = curInterval[1];
             if(curEnd < newStart){
                 list.add(new int[]{curStart, curEnd});
             }else if(newEnd < curStart){
@@ -16,11 +16,6 @@ class Solution {
             }
         }
         list.add(new int[]{newStart, newEnd});
-        int[][] results = new int[list.size()][2];
-        for(int index = 0; index < list.size(); index++){
-            results[index][0] = list.get(index)[0];
-            results[index][1] = list.get(index)[1];
-        }
-        return results;
+        return list.toArray(new int[list.size()][2]);
     }
 }
