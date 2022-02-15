0dd32f7ccfa76b31696bf40448cc63fd0765c044
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-14 18:26:41 -0800

    Time: 3 ms (43.76%), Space: 48.1 MB (6.78%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -16,11 +16,6 @@ class Solution {
             }
         }
         list.add(new int[]{newStart, newEnd});
-        int[][] results = new int[list.size()][2];
-        for(int index = 0; index < list.size(); index++){
-            results[index][0] = list.get(index)[0];
-            results[index][1] = list.get(index)[1];
-        }
-        return results;
+        return list.toArray(new int[list.size()][]);
     }
 }
