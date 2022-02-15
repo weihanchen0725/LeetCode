ff709f3a0a00cf0f93f0d89a5c5e2d418c9a85fd
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-14 19:02:50 -0800

    Time: 77 ms (60.39%), Space: 100.1 MB (61.65%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -3,14 +3,13 @@ class Solution {
         int length = intervals.length;
         if(intervals == null || length <= 1) return 0;
         Arrays.sort(intervals, (a,b) -> a[1] - b[1]);
-        int end = intervals[0][1];
-        int count = 1;
+        int count = 1, end = intervals[0][1];
         for(int index = 1; index < length; index++){
             if(intervals[index][0] >= end){
                 end = intervals[index][1];
                 count++;
             }
         }
-        return length-count;
+        return length - count;
     }
 }
