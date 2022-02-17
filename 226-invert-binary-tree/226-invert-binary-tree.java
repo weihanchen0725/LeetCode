39e4db5e1c01211f6d366e150989c235b3ffc833
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-17 13:30:32 -0800

    Time: 0 ms (100.00%), Space: 41.5 MB (19.98%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -15,11 +15,11 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-       if(root == null) return root;
+        if(root == null) return root;
         invert(root);
         return root;
     }
-    public void invert(TreeNode node){
+    public void invert(TreeNode  node){
         if(node == null) return ;
         TreeNode tempNode = node.left;
         node.left = node.right;
