28507092c5dea03a4b598f477413a51cdad225fc
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-16 17:11:36 -0800

    Time: 0 ms (100.00%), Space: 44.7 MB (72.91%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -14,13 +14,10 @@
  * }
  */
 class Solution {
-    int height(TreeNode root) {
-        return root == null ? -1 : 1 + height(root.left);
-    }
     public int countNodes(TreeNode root) {
-        int h = height(root);
-        return h < 0 ? 0 :
-               height(root.right) == h-1 ? (1 << h) + countNodes(root.right)
-                                         : (1 << h-1) + countNodes(root.left);
+        if(root == null) return 0;
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return 1 + left + right;
     }
 }
