81f94bdd450c5cae883841040ba597d3be359a51
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-17 17:02:25 -0800

    Time: 1 ms (99.09%), Space: 44.8 MB (13.92%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,23 +15,20 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return traverse(inorder,postorder, inorder.length-1,0,postorder.length-1);
+        return build(inorder, postorder, inorder.length-1, 0, postorder.length-1);
     }
-    public TreeNode traverse(int[] inorder, int[] postorder,int inS, int inE,int poS){
-        if(poS<0 || inE>inS){
-            return null;
-        }
-        TreeNode node = new TreeNode(postorder[poS]);
+    public TreeNode build(int[] inorder, int[] postorder, int iStart, int iEnd, int pStart){
+        if(pStart < 0 || iStart < iEnd) return null;
+        TreeNode node = new TreeNode(postorder[pStart]);
         int index = 0;
-        for(int i=inS;i>=inE;i--){
-            if(node.val == inorder[i])
-            {
+        for(int i = iStart; i >= iEnd; i--){
+            if(node.val == inorder[i]){
                 index = i;
                 break;
             }
         }
-        node.right = traverse(inorder,postorder,inS,index+1,poS-1);
-        node.left = traverse(inorder,postorder,index-1,inE,poS -(inS-index)-1);
+        node.right = build(inorder, postorder, iStart, index+1, pStart-1);
+        node.left = build(inorder, postorder, index-1, iEnd, pStart-(iStart-index)-1);
         return node;
     }
 }
