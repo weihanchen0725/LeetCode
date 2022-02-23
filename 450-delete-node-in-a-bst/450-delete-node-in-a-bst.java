a061b65b6784cddcc57341399fb633cc6621b0d0
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-22 16:27:38 -0800

    Time: 0 ms (100.00%), Space: 42.3 MB (79.10%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -14,32 +14,26 @@
  * }
  */
 class Solution {
-    public static TreeNode getSuccessor(TreeNode curr){
-        curr=curr.right;
-        while(curr!=null && curr.left!=null)
-            curr=curr.left;
-        return curr;
-    }
-    public TreeNode deleteNode(TreeNode root, int x) {
-        if(root==null)
-            return root;
-        if(root.val>x)
-            root.left=deleteNode(root.left,x);
-        else if(root.val<x)
-            root.right=deleteNode(root.right,x);
-        else{
-            if(root.left==null){
-                return root.right;
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+        if(root.val == key){
+            if(root.left == null) return root.right;
+            if(root.right == null) return root.left;
+            TreeNode min = root.right;
+            while(min.left != null){
+                min = min.left;
             }
-            else if(root.right==null){
-                return root.left;
-            }
-            else{
-                TreeNode succ=getSuccessor(root);
-                root.val=succ.val;
-                root.right=deleteNode(root.right,succ.val);
-            }
+            root.right = deleteNode(root.right, min.val);
+            min.left = root.left;
+            min.right = root.right;
+            root = min;
         }
-        return root; 
+        else if(root.val < key){
+            root.right = deleteNode(root.right, key);
+        }
+        else if(root.val > key){
+            root.left = deleteNode(root.left, key);
+        }
+        return root;
     }
 }
