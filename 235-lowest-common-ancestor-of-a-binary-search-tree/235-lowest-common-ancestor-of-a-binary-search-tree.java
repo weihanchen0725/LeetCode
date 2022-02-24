17926ee64e73df3bcec8c54ae7bda31e2ae0e19f
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-24 13:01:09 -0800

    Time: 7 ms (54.19%), Space: 49.3 MB (35.53%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -12,8 +12,8 @@ class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode current = root;
         while(current != null){
-            if(current.val < p.val && current.val < q.val) current = current.right;
-            else if(current.val > p.val && current.val > q.val) current = current.left;
+            if(current.val > p.val && current.val > q.val) current = current.left;
+            else if(current.val < p.val && current.val < q.val) current = current.right;
             else break;
         }
         return current;
