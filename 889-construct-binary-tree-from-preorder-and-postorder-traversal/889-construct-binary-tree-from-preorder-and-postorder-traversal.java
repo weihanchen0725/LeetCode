b63f744a2bb191b7aa2290225b1a98b43908a811
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-25 14:49:04 -0800

    Time: 2 ms (55.42%), Space: 44.1 MB (17.77%) - LeetHub

--- 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -14,14 +14,14 @@
  * }
  */
 class Solution {
-        int preIndex = 0, posIndex = 0;
-    public TreeNode constructFromPrePost(int[]pre, int[]post) {
-        TreeNode root = new TreeNode(pre[preIndex++]);
-        if (root.val != post[posIndex])
-            root.left = constructFromPrePost(pre, post);
-        if (root.val != post[posIndex])
-            root.right = constructFromPrePost(pre, post);
-        posIndex++;
-        return root;
+    int preIndex = 0, postIndex = 0;
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        TreeNode node = new TreeNode(preorder[preIndex++]);
+        if(node.val != postorder[postIndex])
+            node.left = constructFromPrePost(preorder, postorder);
+        if(node.val != postorder[postIndex])
+            node.right = constructFromPrePost(preorder, postorder);
+        postIndex++;
+        return node;
     }
 }
