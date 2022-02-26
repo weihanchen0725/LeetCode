11d6d9aa70b0dd238fbde970db40a8664d5dca91
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-25 16:13:36 -0800

    Time: 1 ms (60.76%), Space: 41.6 MB (35.13%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,16 +14,25 @@
  * }
  */
 class Solution {
-        int i = 0;
-    public TreeNode bstFromPreorder(int[] A) {
-        return bstFromPreorder(A, Integer.MAX_VALUE);
-    }
-
-    public TreeNode bstFromPreorder(int[] A, int bound) {
-        if (i == A.length || A[i] > bound) return null;
-        TreeNode root = new TreeNode(A[i++]);
-        root.left = bstFromPreorder(A, root.val);
-        root.right = bstFromPreorder(A, bound);
+    public TreeNode bstFromPreorder(int[] preorder) {
+        TreeNode root = null;
+        for(int i = 0; i < preorder.length; i++) {
+            root = insert(root, preorder[i]);
+        }
         return root;
+    }
+    
+    public static TreeNode insert(TreeNode node, int data) {
+        if(node == null) {
+            return new TreeNode(data);
+        }
+
+        if(node.val < data)
+            node.right = insert(node.right, data);
+
+        if(node.val > data)
+            node.left = insert(node.left, data);
+
+        return node;
     }
 }
