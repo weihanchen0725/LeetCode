1932f691c489b0f5ddb7b506bb11d3040404104e
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-28 14:42:35 -0800

    Time: 0 ms (100.00%), Space: 41.5 MB (50.93%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -10,19 +10,11 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head == null || head.next == null)
-            return head;
-        
+        if(head == null || head.next == null) return head;
         ListNode current = head;
-
-        
-        while(current!=null && current.next!=null)
-        {
-            if(current.val == current.next.val)
-                current.next = current.next.next;
-            else
-                current = current.next;
-        
+        while(current != null && current.next != null){
+            if(current.val == current.next.val) current.next = current.next.next;
+            else current = current.next;
         }
         return head;
     }
