cb616d71ec2d913cfd2b4c3d95f59d78adf2efff
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-28 10:18:43 -0800

    Time: 1 ms (93.95%), Space: 44 MB (9.16%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
@@ -16,15 +16,14 @@
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> results = new ArrayList<>();
-        if(root == null) return results;
         helper(root, results, 0);
         return results;
     }
-    public void helper(TreeNode root, List<List<Integer>> results, int level){
-        if(root == null) return ;
-        if(results.size() <= level) results.add(0, new ArrayList<>());
-        helper(root.left, results, level+1);
-        helper(root.right, results, level+1);
-        results.get(results.size()-level-1).add(root.val);
+    public void helper(TreeNode node, List<List<Integer>> results, int level){
+        if(node == null) return ;
+        if(results.size() <= level) results.add(0, new ArrayList<Integer>());
+        helper(node.left, results, level+1);
+        helper(node.right, results, level+1);
+        results.get(results.size()-level-1).add(node.val);   
     }
 }
