26178793a143f083ef320f75b4080b33ee5a3382
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-02-28 14:20:22 -0800

    Time: 0 ms (100.00%), Space: 42.3 MB (19.92%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -19,7 +19,7 @@ class Solution {
         return construct(preorder, Integer.MAX_VALUE);
     }
     public TreeNode construct(int[] preorder, int max){
-        if(index == preorder.length || preorder[index] > max) return null;
+        if(index == preorder.length || preorder[index] >= max) return null;
         TreeNode node = new TreeNode(preorder[index++]);
         node.left = construct(preorder, node.val);
         node.right = construct(preorder, max);
