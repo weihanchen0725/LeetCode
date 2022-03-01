284f303f091181a1eb18422ba4568958e824850e
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-01 12:09:07 -0800

    Time: 0 ms (100.00%), Space: 45.5 MB (38.31%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -12,13 +12,11 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if(head == null) return false;
-        ListNode slow = head;
-        ListNode fast = head;
+        ListNode slow = head, fast = head;
         while(fast.next != null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
-            if(fast == slow)
-                return true;
+            if(slow == fast) return true;
         }
         return false;
     }
