5fe1c45dc40e9343ea8a4a3b1e51028c891cf5b7
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-01 16:25:17 -0800

    Time: 0 ms (100.00%), Space: 41.6 MB (35.19%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
@@ -9,44 +9,42 @@
  * }
  */
 class Solution {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode temp = null,dup=head;
-        int index = 0,ind=0;
-        while(head!=null){
-            index++;
-            if(index < left || index > right){
-                head = head.next;continue;
-            }
-            else{
-            if(temp == null){
-                temp = new ListNode(head.val);
-            }
-            else{
-            ListNode new_node = new ListNode(head.val);
-            new_node.next = temp;
-            temp = new_node;
-            }
-            head = head.next;
-            }
-        }
-        ListNode ans = new ListNode(0);
-        ListNode curr = ans,x=temp;
-        while(dup!=null){
-            ind++;
-            if(ind<left||ind>right)
-            {
-                curr.next = new ListNode(dup.val);
-                curr = curr.next;
-            }
-            else{
-            
-                curr.next = new ListNode(x.val);
-                curr = curr.next;
-                x = x.next;
-                 }
-            
-            dup = dup.next;
-        }
-        return ans.next;
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+    if (m == n) {
+        return head;
     }
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    int count = 0;
+    ListNode left1 = null;
+    ListNode left2 = null;
+    ListNode pre = dummy;
+    while (head != null) {
+        count++;
+        //到达 m，保存 l1 和 l2
+        if (count == m) {
+            left1 = pre;
+            left2 = head;
+        }
+        // m 和 n 之间，倒转链表
+        if (count > m && count < n) {
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+            continue;
+        }
+        //到达 n
+        if (count == n) {
+            left2.next = head.next;
+            head.next = pre;
+            left1.next = head;
+            break;
+        }
+        //两个指针后移
+        head = head.next;
+        pre = pre.next;
+    }
+    return dummy.next;
+}
 }
