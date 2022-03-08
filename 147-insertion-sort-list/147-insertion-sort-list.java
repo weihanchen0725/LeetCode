2d6d02d5b737a8dab42ecec4568c4951b736e678
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-07 16:28:42 -0800

    Time: 5 ms (80.96%), Space: 44.8 MB (27.52%) - LeetHub

--- 147-insertion-sort-list/147-insertion-sort-list.java
+++ 147-insertion-sort-list/147-insertion-sort-list.java
@@ -8,31 +8,33 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-        public ListNode insertionSortList(ListNode ptr) {    
-        if (ptr == null || ptr.next == null)
-            return ptr;
-        
-        ListNode preInsert, toInsert, dummyHead = new ListNode(0);
-        dummyHead.next = ptr;
 
-        while (ptr != null && ptr.next != null) {
-            if (ptr.val <= ptr.next.val) {
-                ptr = ptr.next;
-            } else {      
-                toInsert = ptr.next;
-                // Locate preInsert.
-                preInsert = dummyHead;
-                while (preInsert.next.val < toInsert.val) {
-                    preInsert = preInsert.next;
-                }
-                ptr.next = toInsert.next;
-                // Insert toInsert after preInsert.
-                toInsert.next = preInsert.next;
-                preInsert.next = toInsert;
-            }
-        }
+//这个题这几本应该是能写出来的，但是有一点学习到了，就是如果做linkedlist时，如果需要两个指针一个指cur，一个指cur前一个，当有这样需求的时候，可以试试cur去替代前一个即pre，而让cur.next去代替cur，这样就可以少写一个指针，且能方便改变前后指针的方向；
+
+
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = head;
+        ListNode pre = dummy;
         
-        return dummyHead.next;
+        while(cur != null && cur.next != null) {
+            if(cur.val < cur.next.val) { // 这里就是用cur.next去替代原本的cur来进行判断； 另外就是这里直接找到第一个当前比下一个小的node，这样就可以保证新找到的cur.next，一定会插到cur的前面，这样cur所指的就一定是已经拍好序列的tail，最后一个node
+                cur = cur.next;
+            } else {
+                pre = dummy;
+                
+                while(pre.next.val < cur.next.val) {
+                    pre = pre.next;
+                }
+                
+                ListNode next = cur.next.next;
+                cur.next.next = pre.next;
+                pre.next = cur.next;
+                cur.next = next;  
+            }
+        }          
+        return dummy.next;
     }
 }
