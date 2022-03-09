20dbb378472e24173380a08ff26519b0799b533a
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-09 13:25:26 -0800

    Time: 1 ms (95.35%), Space: 50.2 MB (60.66%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int answer = 0;
-        for(int num : nums) answer ^= num;
-        return answer;
+        int result = 0;
+        for(int num : nums) result ^= num;
+        return result;
     }
 }
