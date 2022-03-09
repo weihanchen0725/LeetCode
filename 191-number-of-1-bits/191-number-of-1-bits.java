9bae2d4d14582b7f4bf0c846b2b328e0431d67e4
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-09 13:42:57 -0800

    Time: 1 ms (80.40%), Space: 41.4 MB (24.61%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -3,8 +3,8 @@ public class Solution {
     public int hammingWeight(int n) {
         int count = 0;
         while(n != 0){
+            n = n & n - 1;
             count++;
-            n &= n-1;
         }
         return count;
     }
