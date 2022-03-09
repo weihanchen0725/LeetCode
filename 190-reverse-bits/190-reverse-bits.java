f8f2cf2c15ead0f60175585f4ffd82e3a055ae72
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-09 11:32:15 -0800

    Time: 2 ms (39.74%), Space: 42.2 MB (44.94%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -1,14 +1,14 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int result = 0;
-        for(int index = 0; index < 32; index++){
-            result += n & 1;
-            n >>>= 1;
-            if(index < 31){
-                result <<= 1;
-            }
+        int res = 0;
+        
+        for(int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>= 1;
         }
-        return result;
+        
+        return res;
     }
 }
