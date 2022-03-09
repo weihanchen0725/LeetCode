652f2942ac4fdc5cdd2779b623c3179160811d1f
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-09 13:41:50 -0800

    Time: 1 ms (99.96%), Space: 56.5 MB (24.87%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,22 +1,11 @@
 class Solution {
-
-public int majorityElement(int[] nums) {
-        return recurse(nums, 0);
-    }
-    
-    public int recurse(int[] nums, int start){
-        int n = nums.length;
-        int tracker= 1;
-        for(int i=start+1;i<n;i++){
-            if(nums[start]==nums[i]){
-                tracker++;
-            }else{
-                tracker--;
-            }
-            if(tracker==0){
-                return recurse(nums, i+1);
-            }
+    public int majorityElement(int[] nums) {
+        int count = 0, candidate = 0;
+        for(int num : nums){
+            if(count == 0) candidate = num;
+            if(candidate == num) count++;
+            else count--;
         }
-        return nums[start];
+        return candidate;
     }
 }
