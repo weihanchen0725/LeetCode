9c7ebf918acaa671ec51d6834ee91c3b3a21fde5
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-09 14:11:41 -0800

    Time: 1 ms (66.02%), Space: 41.1 MB (44.01%) - LeetHub

--- 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.java
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.java
@@ -1,16 +1,55 @@
-public class Solution {
-    
-    char[] map = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
-    
+class Solution {
     public String toHex(int num) {
-        if(num == 0) return "0";
-        String result = "";
-        while(num != 0){
-            result = map[(num & 15)] + result; 
-            num = (num >>> 4);
+        // Safety 
+        if(num == 0)
+        {
+            return "0";
         }
-        return result;
-    }
+        
+        int defVal = 0;
+        
+        // if the num is negative
+        // using twos complement  
+        if(num < 0)
+        {
+            num = (-1 * num) - 1;
+            defVal = 15;
+        }
+        
+        String ans = convertToHex(num, defVal);
+        
+        return ans; 
+        
+    } // end of toHex method 
     
-    
-}
+    private String convertToHex(int num, int defVal)
+    {
+        char[] charArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b','c','d','e','f'};
+        StringBuilder ans = new StringBuilder();
+        
+        while(num > 0)
+        {
+            int result = num % 16;
+            
+            if(defVal > 0)
+            {
+                result = defVal - num % 16;
+            }
+            
+            ans.append(charArray[result]);
+            
+            // decrement the num 
+            num = num / 16;
+        } // end of while loop
+        
+        if(defVal > 0)
+        {
+            while(ans.length() < 8)
+            {
+                ans.append(charArray[defVal]);
+            }
+        }
+        
+        return ans.reverse().toString();
+    } // end of convertToHex method 
+} // end of Solution class
