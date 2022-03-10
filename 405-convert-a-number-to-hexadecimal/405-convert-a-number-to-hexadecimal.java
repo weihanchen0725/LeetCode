7e2c43e20d5b668c578b1b5aa3020734585d6375
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-09 16:38:19 -0800

    Time: 0 ms (100.00%), Space: 41.9 MB (20.31%) - LeetHub

--- 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.java
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.java
@@ -1,23 +1,21 @@
 class Solution {
     public String toHex(int num) {
         if(num == 0) return "0";
-        
         int defVal = 0;
-        
         if(num < 0){
+            num = (num * -1) - 1;
             defVal = 15;
-            num = (-1 * num) - 1;
         }
-        
-        return convertHex(num, defVal);
+        return convert(num, defVal);
     }
-    public String convertHex(int num, int defVal){
+    public String convert(int num, int defVal){
         char[] charArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
         StringBuilder sb = new StringBuilder();
+        
         while(num > 0){
-            int result = (defVal > 0) ? (defVal - num % 16) : num % 16;
-            sb.append(charArray[result]);
-            num /= 16;
+            int hex = (defVal > 0) ? defVal - num % 16 : num % 16;
+            sb.append(charArray[hex]);
+            num = num / 16;
         }
         if(defVal > 0)
             while(sb.length() < 8)
