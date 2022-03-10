71f440647b3320bcaa5d4a76f39c6ce22615af3a
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-09 18:43:25 -0800

    Time: 0 ms (100.00%), Space: 41.5 MB (14.09%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.java
+++ 461-hamming-distance/461-hamming-distance.java
@@ -1,6 +1,6 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int xor = x^y, count = 0;
+        int xor = x ^ y, count = 0;
         while(xor > 0){
             if((xor&1) == 1) count++;
             xor >>= 1;
