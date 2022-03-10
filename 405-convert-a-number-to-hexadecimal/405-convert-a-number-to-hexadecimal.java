828f12a8c6a24cfc383a2c38bca54a07c6240bc2
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-09 16:17:09 -0800

    Time: 1 ms (66.02%), Space: 41.6 MB (25.00%) - LeetHub

--- 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.java
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.java
@@ -1,55 +1,28 @@
 class Solution {
     public String toHex(int num) {
-        // Safety 
-        if(num == 0)
-        {
-            return "0";
-        }
+        if(num == 0) return "0";
         
         int defVal = 0;
         
-        // if the num is negative
-        // using twos complement  
-        if(num < 0)
-        {
-            num = (-1 * num) - 1;
+        if(num < 0){
             defVal = 15;
+            num = (-1 * num) - 1;
         }
         
-        String ans = convertToHex(num, defVal);
-        
-        return ans; 
-        
-    } // end of toHex method 
-    
-    private String convertToHex(int num, int defVal)
-    {
-        char[] charArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b','c','d','e','f'};
-        StringBuilder ans = new StringBuilder();
-        
-        while(num > 0)
-        {
-            int result = num % 16;
-            
-            if(defVal > 0)
-            {
-                result = defVal - num % 16;
-            }
-            
-            ans.append(charArray[result]);
-            
-            // decrement the num 
-            num = num / 16;
-        } // end of while loop
-        
+        return convertHex(num, defVal);
+    }
+    public String convertHex(int num, int defVal){
+        char[] charArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        StringBuilder sb = new StringBuilder();
+        while(num > 0){
+            int result = (defVal > 0) ? (defVal - num % 16) : num % 16;
+            sb.append(charArray[result]);
+            num /= 16;
+        }
         if(defVal > 0)
-        {
-            while(ans.length() < 8)
-            {
-                ans.append(charArray[defVal]);
-            }
-        }
+            while(sb.length() < 8)
+                sb.append(charArray[defVal]);
         
-        return ans.reverse().toString();
-    } // end of convertToHex method 
-} // end of Solution class
+        return sb.reverse().toString();
+    }
+}
