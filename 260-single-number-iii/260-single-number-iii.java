3da1bff3ccfbe135d0fb24af020c21b1b148332e
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-10 18:37:03 -0800

    Time: 2 ms (54.81%), Space: 44.8 MB (47.69%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.java
+++ 260-single-number-iii/260-single-number-iii.java
@@ -1,21 +1,12 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        // bit operation
-        // need to understand and remember
-        // not easy to understand
-        // hash table also works
-        
-        // difference between two numbers (x and y) which were seen only once
         int bitmask = 0;
-        for (int num : nums) bitmask ^= num;
-
-        // rightmost 1-bit diff between x and y
-        int diff = bitmask & (-bitmask);
-
-        int x = 0;
-        // bitmask which will contain only x
-        for (int num : nums) if ((num & diff) != 0) x ^= num;
-
-        return new int[]{x, bitmask^x};
+        for(int num : nums)
+            bitmask ^= num;
+        int x = 0, diff = bitmask & (-bitmask);
+        for(int num : nums)
+            if((num & diff) != 0)
+                x ^= num;
+        return new int[]{x, bitmask ^ x};
     }
 }
