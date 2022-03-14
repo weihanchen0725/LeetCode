ca4ca87bba670061964e28e64c991966c270a608
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-14 12:52:47 -0700

    Time: 1 ms (82.36%), Space: 43.4 MB (44.50%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> results = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-        backtrack(nums, current, results, 0);
+        List<Integer> temp = new ArrayList<>();
+        helper(nums, temp, results, 0);
         return results;
     }
-    public void backtrack(int[] nums, List<Integer> current, List<List<Integer>> results, int index){
+    public void helper(int[] nums, List<Integer> temp, List<List<Integer>> results, int index){
         if(index == nums.length){
-            results.add(new ArrayList<>(current));
+            results.add(new ArrayList<>(temp));
             return ;
         }
-        backtrack(nums, current, results, index+1);
-        current.add(nums[index]);
-        backtrack(nums, current, results, index+1);
-        current.remove(current.size()-1);
+        helper(nums, temp, results, index+1);
+        temp.add(nums[index]);
+        helper(nums, temp, results, index+1);
+        temp.remove(temp.size()-1);
     }
 }
