ed125488fb0ed8396d6246b4bc057bcc1fb1c0e9
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-14 17:00:41 -0700

    Time: 1 ms (91.79%), Space: 43.7 MB (58.16%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.java
+++ 137-single-number-ii/137-single-number-ii.java
@@ -1,18 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        // x: 1100
-        // ~y: 0011
-        // if x and y are the same, then x & ~y == 0
-        
-        
-        int seenOnce = 0;
-        int seenTwice = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            seenOnce = (~seenTwice) & (seenOnce ^ nums[i]);
-            seenTwice = (~seenOnce) & (seenTwice ^ nums[i]);            
+        int seenOnce = 0, seenTwice = 0;
+        for(int num : nums){
+            seenOnce = (~seenTwice) & (seenOnce^num);
+            seenTwice = (~seenOnce) & (seenTwice^num);
         }
-        
         return seenOnce;
     }
 }
