18ff9808e7f7c5b7f81d3fdd19bda78098d758fe
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-14 19:13:02 -0700

    Time: 8 ms (76.89%), Space: 55.5 MB (47.70%) - LeetHub

--- 89-gray-code/89-gray-code.java
+++ 89-gray-code/89-gray-code.java
@@ -2,9 +2,8 @@ class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer> results = new ArrayList<>();
         if(n == 0) results.add(0);
-        for(int index = 0; index < (1 << n); index++){
+        for(int index = 0; index < (1 << n); index++)
             results.add(index^(index>>1));
-        }
         return results;
     }
 }
