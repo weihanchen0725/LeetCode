c4aa59e097529823b0953265bdaec4aa982b6ba0
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-14 19:29:36 -0700

    Time: 0 ms (100.00%), Space: 40.9 MB (33.54%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.java
+++ 371-sum-of-two-integers/371-sum-of-two-integers.java
@@ -1,9 +1,9 @@
 class Solution {
     public int getSum(int a, int b) {
         while(b != 0){
-            int temp = (a ^ b);
-            b = (a & b) << 1;
-            a = temp;
+            int temp = (a&b) << 1;
+            a ^= b;
+            b = temp;
         }
         return a;
     }
