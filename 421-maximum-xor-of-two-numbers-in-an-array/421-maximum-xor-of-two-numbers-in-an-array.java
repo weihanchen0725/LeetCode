c42fd12d6c7ce239405918481968b0cca1a2f170
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-15 18:05:39 -0700

    Time: 260 ms (54.91%), Space: 65.8 MB (87.11%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
@@ -3,13 +3,12 @@ class Solution {
         int max = 0, mask = 0;
         for(int index = 31; index >= 0; index--){
             mask = mask | (1 << index);
-            Set<Integer> set = new HashSet<>();
-            for(int num : nums){
+            HashSet<Integer> set = new HashSet<>();
+            for(int num : nums)
                 set.add(num & mask);
-            }
             int temp = max | (1 << index);
-            for(int prefix : set){
-                if(set.contains(prefix^temp)){
+            for(int num : set){
+                if(set.contains(num ^ temp)){
                     max = temp;
                     break;
                 }
