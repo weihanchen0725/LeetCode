c9f158151bbdd3caac02b018ce1a5e6a475f2ed1
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-15 19:10:38 -0700

    Time: 0 ms (100.00%), Space: 51.7 MB (14.94%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,8 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int length = nums.length;
-        for(int index = 1; index < length; index++)
+        for(int index = 1; index < length; index++){
             nums[index] += nums[index-1];
-        return length * (length+1)/2 - nums[length-1];
+        }
+        return length*(length+1)/2-nums[length-1];
     }
-}
+} 
