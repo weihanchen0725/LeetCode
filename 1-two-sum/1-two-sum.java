b7751c2e1695ab2eeb980d85d7eba2cfdad8df31
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-24 19:27:47 -0700

    Time: 5 ms (60.28%), Space: 47.6 MB (5.75%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,11 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> dictionary = new HashMap<>();
+        int length = nums.length;
         int[] results = new int[2];
-        for(int index = 0; index < nums.length; index++){
-            if(dictionary.containsKey(target-nums[index])){
-                results[0] = (index < dictionary.get(target-nums[index])) ? index : dictionary.get(target-nums[index]);
-                results[1] = (index > dictionary.get(target-nums[index])) ? index : dictionary.get(target-nums[index]);
+        HashMap<Integer, Integer> dictionary = new HashMap<>();
+        for(int index = 0; index < length; index++){
+            int currentTarget = target - nums[index];
+            if(dictionary.containsKey(currentTarget)){
+                int currentTargetIndex = dictionary.get(currentTarget);
+                results[0] = (currentTargetIndex < index) ? currentTargetIndex : index;
+                results[1] = (currentTargetIndex > index) ? currentTargetIndex : index;
             }
             dictionary.put(nums[index], index);
         }
