4dfcd0099ce569b88cdc5477e2fd575c7dd418e2
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-24 19:30:21 -0700

    Time: 20 ms (96.45%), Space: 41.9 MB (93.74%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,20 +1,22 @@
 class Solution {
     public int low = 0, high = 1;
     public String longestPalindrome(String s) {
-        for(int index = 0; index < s.length(); index++){
+        int length = s.length();
+        for(int index = 0; index < length; index++){
             isPalindrome(s, index, index);
             isPalindrome(s, index, index+1);
         }
-        return s.substring(low, low + high);
+        return s.substring(low, low+high);
     }
     public void isPalindrome(String str, int left, int right){
-        while(left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)){
+        int length = str.length();
+        while(left >= 0 && right < length && str.charAt(left) == str.charAt(right)){
             left--;
             right++;
         }
-        if(high < right-left-1){
-            low = left+1;
-            high = right-left-1;
+        if(high < right - left - 1){
+            low = left + 1;
+            high = right - left - 1;
         }
     }
 }
