3308e48bc4d66a69d83c633e3e4ca181bb2057e8
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-29 15:40:44 -0700

    Time: 2 ms (80.62%), Space: 48.5 MB (26.61%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
         int[] results = new int[n+1];
-        for(int index = 0; index < results.length; index++)
+        for(int index = 0; index < results.length; index++){
             results[index] = results[index>>1] + (index&1);
+        }
         return results;
     }
 }
