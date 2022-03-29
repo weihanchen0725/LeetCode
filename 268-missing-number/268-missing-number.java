ac180eea46e79fc8e0e27605bb0bac733c8c668a
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-29 15:38:08 -0700

    Time: 1 ms (70.78%), Space: 51 MB (62.07%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -4,6 +4,6 @@ class Solution {
         for(int index = 1; index < length; index++){
             nums[index] += nums[index-1];
         }
-        return length * (length+1) / 2 - nums[length-1];
+        return length * (length+1)/2 - nums[length-1];
     }
 }
