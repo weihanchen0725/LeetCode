f8767de7d8a06174e4703117bd168ce17829bf47
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-29 18:55:38 -0700

    Time: 0 ms (100.00%), Space: 39.3 MB (82.33%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
         int result = 0, ab = a|b, equal = ab^c;
-        for(int index = 0; index < 32; index++){
+        for(int index = 0; index <32; index++){
             int bitmask = 1 << index;
             if((bitmask & equal) > 0){
                 result += ((a&bitmask) == (b&bitmask) && (c&bitmask) == 0) ? 2 : 1;
