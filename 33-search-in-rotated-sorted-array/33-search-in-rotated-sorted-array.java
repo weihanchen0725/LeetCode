855305d1d4eb8f39b4791fcb8f96e0fff7f83807
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-31 16:43:43 -0700

    Time: 0 ms (100.00%), Space: 42.9 MB (26.87%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,25 +1,17 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
+        int left = 0, right = nums.length-1;
         while(left <= right){
             int mid = left + (right - left) / 2;
-            if(nums[mid] == target) return mid;
+            if(target == nums[mid]) return mid;
             if(nums[mid] >= nums[left]){
-                if(target > nums[mid]){
-                    left = mid + 1;
-                }else if(target >= nums[left]){
-                    right = mid - 1;
-                }else{
-                    left = mid + 1;
-                }
+                if(target > nums[mid]) left = mid + 1;
+                else if(target >= nums[left]) right = mid - 1;
+                else left = mid + 1;
             }else{
-                if(target < nums[mid]){
-                    right = mid - 1;
-                }else if(target <= nums[right]){
-                    left = mid + 1;
-                }else{
-                    right = mid - 1;
-                }
+                if(target < nums[mid]) right = mid - 1;
+                else if(target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
             }
         }
         return -1;
