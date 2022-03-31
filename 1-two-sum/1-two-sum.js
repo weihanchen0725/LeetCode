b93b288fdb5ba63f34061f771c2d16fdbbb90d92
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-31 12:04:48 -0700

    Time: 100 ms (62.52%), Space: 42.9 MB (41.26%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,11 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = {};
-    for(let index = 0; index < nums.length; index++){
-        let anotherTarget = target - nums[index];
-        if(anotherTarget in map)
+    var map = {};
+    for(var index = 0; index < nums.length; index++){
+        var anotherTarget = target - nums[index];
+        if(anotherTarget in map){
             return [map[anotherTarget], index];
+        }
         map[nums[index]] = index;
     }
     return null;
