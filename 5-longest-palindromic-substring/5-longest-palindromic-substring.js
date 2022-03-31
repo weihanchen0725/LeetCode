a5dcb1c6a67f9f7ab759ddd8ccfa4da8af36932f
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-03-30 19:36:48 -0700

    Time: 219 ms (48.25%), Space: 44.3 MB (82.42%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -1,29 +1,20 @@
-// Expand Around Center solution
-// T O(N^2)
-// S O(1)
+/**
+ * @param {string} s
+ * @return {string}
+ */
 var longestPalindrome = function(s) {
-  var max = '';
-  for (var i = 0; i < s.length; i++) {
-    // this loop is to take into account 
-    // different palindromes like 'aba' and 'abba'
-    for (var j = 0; j < 2; j++) {
-      var left = i;
-      var right = i + j;
-      while (s[left] && s[left] === s[right]) {
-        left--;
-        right++;
-      }
-      // here imagine we get into string like
-      // "sabbad", then
-      // right = 5
-      // left = 0
-      // and actual length of "abba" should be "4"
-      // 5 - 0 - 1 === 4
-      if ((right - left - 1) > max.length) {
-        max = s.substring(left + 1, right);
-      }
-        
+    var max = '';
+    for(var index = 0; index < s.length; index++){
+        for(var jIndex = 0; jIndex < 2; jIndex++){
+            var left = index, right = index + jIndex;
+            while(s[left] && s[left] === s[right]){
+                left--;
+                right++;
+            }
+            if((right-left-1) > max.length){
+                max = s.substring(left+1, right);
+            }
+        }
     }
-  }
-  return max;
+    return max;
 };
