482d62667d5a99840b1266c1e82c3ed38c8175bc
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-04 18:44:07 -0700

    Time: 608 ms (19.22%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,2 +1,7 @@
 # Write your MySQL query statement below
-select name as Customers from customers where id not in (select customerid from orders);
+SELECT name AS Customers
+FROM customers
+WHERE id NOT IN(
+SELECT customerId 
+FROM orders
+);
