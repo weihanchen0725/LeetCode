b1268dc304baf62aafe8ca0c0b440321e5e80c7c
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-05 12:21:00 -0700

    Time: 323 ms (21.02%), Space: 51.3 MB (45.83%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,20 +3,17 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if(x<0){
+    if(x < 0){
         return false;
     }
-    var flag = true
-    var val = String(x).split("")
-    
-    for(var i=0; i< val.length / 2; i++)
-    {
-        if(val[i] == val[val.length-i-1]){
-            flag = true;
-        }
-        else{
+    var result = true;
+    var stringX = String(x).split("");
+    for(var index = 0; index < stringX.length/2; index++){
+        if(stringX[index] == stringX[stringX.length-index-1]){
+            result = true;
+        }else{
             return false;
         }
     }
-    return flag;
+    return result;
 };
