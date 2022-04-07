8565e8694e3a5bdb0d2a412098d04d3b8e6ed47b
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-06 17:11:40 -0700

    Time: 1 ms (35.18%), Space: 42.2 MB (73.44%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int length = nums.length, current = 0;
+        int left = 0, right = length - 1;
+        while(current <= right){
+            if(nums[current] == 0){
+                swap(nums, left, current);
+                left++;
+                current++;
+            }else if(nums[current] == 2){
+                swap(nums, right, current);
+                right--;
+            }else{
+                current++;
+            }
+        }
+    }
+    private void swap(int[] nums, int index1, int index2){
+        int tempNum = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = tempNum;
     }
 }
