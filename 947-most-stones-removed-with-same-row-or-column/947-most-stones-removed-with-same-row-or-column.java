fd3ce86050e267f9b4b2f88c11024e575a4fa582
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-12 17:32:42 -0700

    Time: 8 ms (96.56%), Space: 42.9 MB (84.93%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.java
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.java
@@ -1,27 +1,23 @@
 class Solution {
-    Map<Integer, Integer> f = new HashMap<>();
-    int islands = 0;
-
+    Map<Integer, Integer> map = new HashMap<>();
+    int lands = 0;
     public int removeStones(int[][] stones) {
-        for (int i = 0; i < stones.length; ++i)
-            union(stones[i][0], ~stones[i][1]);
-        return stones.length - islands;
+        for(int index = 0; index < stones.length; index++){
+            union(stones[index][0], ~stones[index][1]);
+        }
+        return stones.length-lands;
     }
-
-    public int find(int x) {
-        if (f.putIfAbsent(x, x) == null)
-            islands++;
-        if (x != f.get(x))
-            f.put(x, find(f.get(x)));
-        return f.get(x);
+    public int find(int num){
+        if(map.putIfAbsent(num, num) == null) lands++;
+        if(num != map.get(num)) map.put(num, find(map.get(num)));
+        return map.get(num);
     }
-
-    public void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x != y) {
-            f.put(x, y);
-            islands--;
+    public void union(int num1, int num2){
+        num1 = find(num1);
+        num2 = find(num2);
+        if(num1 != num2){
+            map.put(num1, num2);
+            lands--;
         }
     }
 }
