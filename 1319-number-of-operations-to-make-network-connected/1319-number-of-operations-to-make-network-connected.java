92aa95c2b4ae9c54cdcfea0df359614d4b434c16
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-13 17:22:25 -0700

    Time: 3 ms (99.02%), Space: 60.3 MB (88.77%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -2,20 +2,30 @@ class Solution {
     public int makeConnected(int n, int[][] connections) {
         if (connections.length < n - 1) return -1; // To connect all nodes need at least n-1 edges
         int[] parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
+        int[] size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
         int components = n;
         for (int[] c : connections) {
             int p1 = findParent(parent, c[0]);
             int p2 = findParent(parent, c[1]);
             if (p1 != p2) {
-                parent[p1] = p2; // Union 2 component
+                if (size[p1] < size[p2]) { // Merge small size to large size
+                    size[p2] += size[p1];
+                    parent[p1] = p2;
+                } else {
+                    size[p1] += size[p2];
+                    parent[p2] = p1;
+                }
                 components--;
             }
         }
         return components - 1; // Need (components-1) cables to connect components together
     }
     private int findParent(int[] parent, int i) {
-        while (i != parent[i]) i = parent[i];
-        return i; // Without Path Compression
+        if (i == parent[i]) return i;
+        return parent[i] = findParent(parent, parent[i]); // Path compression
     }
 }
