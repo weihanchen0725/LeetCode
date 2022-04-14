0a2073821882795ef54846b1775039b0ca6c8afa
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-13 19:23:37 -0700

    Time: 1 ms (94.69%), Space: 41.6 MB (92.66%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -1,18 +1,19 @@
 class Solution {
-    int[] uf = new int[26];
+    int[] dictionary = new int[26];
     public boolean equationsPossible(String[] equations) {
-        for (int i = 0; i < 26; ++i) uf[i] = i;
-        for (String e : equations)
-            if (e.charAt(1) == '=')
-                uf[find(e.charAt(0) - 'a')] = find(e.charAt(3) - 'a');
-        for (String e : equations)
-            if (e.charAt(1) == '!' && find(e.charAt(0) - 'a') == find(e.charAt(3) - 'a'))
-                return false;
+        for(int label = 0; label < 26; label++)
+            dictionary[label] = label;
+        for(String equation : equations)
+            if(equation.charAt(1) == '=')
+                dictionary[find(equation.charAt(0)-'a')] = find(equation.charAt(3)-'a');
+        for(String equation : equations)
+            if(equation.charAt(1) == '!' && find(equation.charAt(0)-'a') == find(equation.charAt(3)-'a'))
+               return false;
         return true;
     }
-
-    public int find(int x) {
-        if (x != uf[x]) uf[x] = find(uf[x]);
-        return uf[x];
+    public int find(int num){
+        if(num != dictionary[num])
+            dictionary[num] = find(dictionary[num]);
+        return dictionary[num];
     }
 }
