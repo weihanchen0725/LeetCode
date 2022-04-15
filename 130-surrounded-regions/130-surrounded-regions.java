e8c5a25bf675b06cc0872e59339c45612c348eff
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-15 10:09:16 -0700

    Time: 2 ms (78.99%), Space: 44.5 MB (84.22%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -15,12 +15,7 @@ class Solution {
             for (int j = 0; j < n; ++j) {
                 if (board[i][j] != 'Z') {
                     board[i][j] = 'X';
-                }
-            }
-        }
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (board[i][j] == 'Z') {
+                }else if(board[i][j] == 'Z'){
                     board[i][j] = 'O';
                 }
             }
