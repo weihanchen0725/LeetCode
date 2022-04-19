e445fa8e073aa2226820e207b28be472e2d749a0
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-19 16:01:33 -0700

    Time: 3 ms (91.45%), Space: 44.1 MB (41.55%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -2,13 +2,9 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
         int[] dictionary = new int[26];
-        for(char c : s.toCharArray())
-            dictionary[c-'a']++;
-        for(char c : t.toCharArray())
-            dictionary[c-'a']--;
-        for(int num : dictionary)
-            if(num > 0)
-                return false;
+        for(char sChar : s.toCharArray()) dictionary[sChar-'a']++;
+        for(char tChar : t.toCharArray()) dictionary[tChar-'a']--;
+        for(int num : dictionary) if(num != 0) return false;
         return true;
     }
 }
