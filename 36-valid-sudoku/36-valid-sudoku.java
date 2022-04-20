48127e8d4bee8d9086af9c4fab3a528ffcefdbe5
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-20 16:58:06 -0700

    Time: 1 ms (100.00%), Space: 42.7 MB (81.95%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,53 +1,36 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // validate row / column
-        for (int i = 0; i < 9; i++) {
+        for(int i = 0; i < 9; i++){
             int[] rowFlags = new int[10];
-            int[] colFlags = new int[10];
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    if (rowFlags[board[i][j]-'0'] == 1) {
-                        return false;
-                    } else {
-                        rowFlags[board[i][j]-'0'] = 1;
-                    }
+            int[] columnFlags = new int[10];
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    if(rowFlags[board[i][j]-'0'] == 1) return false;
+                    else rowFlags[board[i][j]-'0'] = 1;
                 }
-                
-                if (board[j][i] != '.') {
-                    if (colFlags[board[j][i]-'0'] == 1) {
-                        return false;
-                    } else {
-                        colFlags[board[j][i]-'0'] = 1;
-                    }
+                if(board[j][i] != '.'){
+                    if(columnFlags[board[j][i]-'0'] == 1) return false;
+                    else columnFlags[board[j][i]-'0'] = 1;
                 }
             }
         }
-        
-        // validate subbox
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                boolean res = checkSubBox(i, j, board);
-                if (!res) {
-                    return false;
-                }
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                boolean result = checkSubBoxes(i, j, board);
+                if(!result) return false;
             }
         }
-        
         return true;
     }
-    
-    private boolean checkSubBox(int i, int j, char[][]board) {
-        int[] flag = new int[10];
-        
-        for (int m = i*3; m < (i+1)*3; m++) {
-            for (int n = j*3; n < (j+1)*3; n++) {
-                if (board[m][n] != '.') {
-                    if (flag[board[m][n]-'0'] == 1) {
-                        return false;
-                    }
-                    flag[board[m][n]-'0'] = 1;
+    public boolean checkSubBoxes(int i, int j, char[][] board){
+        int[] flags = new int[10];
+        for(int m = i*3; m < (i+1)*3; m++){
+            for(int n = j*3; n < (j+1)*3; n++){
+                if(board[m][n] != '.'){
+                    if(flags[board[m][n]-'0'] == 1) return false;
+                    else flags[board[m][n]-'0'] = 1;
                 }
-            }    
+            }
         }
         return true;
     }
