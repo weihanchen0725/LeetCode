8a1556c070ae8dc80e0b08fe0f88d5a9f386e6de
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-19 17:09:16 -0700

    Time: 10 ms (79.25%), Space: 55.8 MB (50.04%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,13 +1,13 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> results = new HashMap<>();
+        HashMap<String, List<String>> dictionary = new HashMap<>();
         for(String str : strs){
-            char[] charSTR = str.toCharArray();
-            Arrays.sort(charSTR);
-            String anagram = String.valueOf(charSTR);
-            results.putIfAbsent(anagram, new ArrayList<String>());
-            results.get(anagram).add(str);
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String anagram = String.valueOf(charArray);
+            dictionary.putIfAbsent(anagram, new ArrayList<String>());
+            dictionary.get(anagram).add(str);
         }
-        return new ArrayList<>(results.values());
+        return new ArrayList<>(dictionary.values());
     }
 }
