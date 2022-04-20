ad5b1290ce56a08481edcedc8865e0f5cb29f5ee
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-19 17:52:38 -0700

    Time: 12 ms (79.21%), Space: 45.2 MB (78.04%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        if(nums == null || k <= 0) return new int[0];
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num : nums) map.put(num, map.getOrDefault(num, 0) +1);
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
-        for(int key : map.keySet()){
-            pq.offer(key);
-            if(pq.size() > k){
-                pq.poll();
+        for(int num : nums)
+            map.put(num, map.getOrDefault(num,0)+1);
+        PriorityQueue<Integer> que = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
+        for(int num : map.keySet()){
+            que.offer(num);
+            if(que.size() > k){
+                que.poll();
             }
         }
         int[] results = new int[k];
         for(int index = 0; index < results.length; index++){
-            results[index] = pq.poll();
+            results[index] = que.poll();
         }
         return results;
     }
