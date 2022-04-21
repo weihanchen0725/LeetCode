4882ecb1615a5e740bda2bcac070334bc37a572f
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-20 17:42:47 -0700

    Time: 10 ms (72.99%), Space: 73 MB (51.66%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -1,24 +1,15 @@
 class Solution {
-    public boolean checkValid(int[][]arr) {
-        int n=arr.length;
-		for(int r=0;r<n;r++)
-		{
-			boolean[] row =new boolean[n+1];
-			boolean[] col =new boolean[n+1];
-			for(int c=0;c<n;c++)
-			{
-				int x=arr[r][c];
-				int y=arr[c][r];
-				
-				if(row[x]==true||col[y]==true)
-				{
-					return false;
-					//sbreak;
-				}
-				row[x]=true;
-				col[y]=true;
-			}
-		}
-		return true;
-	}
+    public boolean checkValid(int[][] matrix) {
+        int length = matrix.length;
+        for(int r = 0; r < length; r++){
+            boolean[] rows = new boolean[length+1];
+            boolean[] columns = new boolean[length+1];
+            for(int c = 0; c < length; c++){
+                if(rows[matrix[r][c]] || columns[matrix[c][r]]) return false;
+                rows[matrix[r][c]] = true;
+                columns[matrix[c][r]] = true;
+            }
+        }
+        return true;
+    }
 }
