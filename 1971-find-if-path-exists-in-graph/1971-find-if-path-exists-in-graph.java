dd9824bc5da5aa4284c76c21a508f8c4134ef5ca
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-26 14:26:49 -0700

    Time: 3 ms (99.80%), Space: 96.9 MB (99.44%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,45 +1,20 @@
-class DisjointSetUnion{
-    private int[] parent;
-    private int N;
-    
-    public DisjointSetUnion(int n){
-        this.N = n;
-        this.parent = new int[this.N];
-        for(int i = 0; i < this.N; i++){
-            this.parent[i] = i;
-        }
-    }
-    
-    public boolean areConnected(int u, int v){
-        return find(u) == find(v);
-    }
-    
-    public void union(int u, int v){
-        if(u != v){
-            int a = find(u);
-            int b = find(v);
-            parent[a] = b;
-        }
-    }
-    
-    private int find(int u){
-        int x = u;
-        while(x != this.parent[x]){
-            x = this.parent[x];
-        }
-        
-        this.parent[u] = x;
-        return x;
-    }
-}
-
 class Solution {
-    public boolean validPath(int n, int[][] edges, int start, int end) {
-        DisjointSetUnion set = new DisjointSetUnion(n);
-        for(int[] edge : edges){
-            set.union(edge[0], edge[1]);
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        boolean[] used = new boolean[n];
+        used[source] = true;
+        boolean newUsedFound = true;
+        while(!used[destination] && newUsedFound){
+            newUsedFound = false;
+            for(int index = edges.length-1; index >= 0; index--){
+                if(used[edges[index][0]]){
+                    if(!used[edges[index][1]]){
+                        newUsedFound = used[edges[index][1]] = true;
+                    }
+                }else if(used[edges[index][1]]){
+                    newUsedFound = used[edges[index][0]] = true;
+                }
+            }
         }
-        
-        return set.areConnected(start, end);
+        return used[destination];
     }
 }
