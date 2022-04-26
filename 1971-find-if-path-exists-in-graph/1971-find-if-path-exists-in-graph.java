a5a982b23ece47204add8700db0c66edea98e22d
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-26 15:04:39 -0700

    Time: 3 ms (99.80%), Space: 137.6 MB (81.20%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -2,16 +2,15 @@ class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         boolean[] used = new boolean[n];
         used[source] = true;
-        boolean newUsedFound = true;
-        while(!used[destination] && newUsedFound){
-            newUsedFound = false;
+        boolean tempUsed = true;
+        while(!used[destination] && tempUsed){
+            tempUsed = false;
             for(int index = edges.length-1; index >= 0; index--){
                 if(used[edges[index][0]]){
-                    if(!used[edges[index][1]]){
-                        newUsedFound = used[edges[index][1]] = true;
-                    }
+                    if(!used[edges[index][1]])
+                        tempUsed = used[edges[index][1]] = true;
                 }else if(used[edges[index][1]]){
-                    newUsedFound = used[edges[index][0]] = true;
+                    tempUsed = used[edges[index][0]] = true;
                 }
             }
         }
