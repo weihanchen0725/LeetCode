1824cf2ee42c51cfdeeb5ba54d0a531f74a242c0
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-26 13:30:27 -0700

    Time: 2 ms (99.87%), Space: 72.3 MB (41.74%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int findJudge(int N, int[][] trust) {
-        int[] count = new int[N+1];
-        for (int[] t: trust) {
-            count[t[0]]--;
-            count[t[1]]++;
+    public int findJudge(int n, int[][] trust) {
+        int[] counts = new int[n+1];
+        for(int[] t : trust){
+            counts[t[0]]--;
+            counts[t[1]]++;
         }
-        for (int i = 1; i <= N; ++i) {
-            if (count[i] == N - 1) return i;
-        }
+        for(int index = 1; index < counts.length; index++)
+            if(counts[index] == n-1)
+                return index;
         return -1;
     }
 }
