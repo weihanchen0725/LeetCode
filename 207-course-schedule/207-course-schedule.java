522f5dc2dd4022e31461574e2c540fe1267a02b9
WEI HAN CHEN <weihanchen0725@gmail.com>
2022-04-26 17:42:40 -0700

    Time: 2 ms (99.78%), Space: 42 MB (99.15%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -11,16 +11,15 @@ class Solution {
             map.get(course).add(must);
         }
         int[] visited = new int[numCourses];
-        for(int index = 0; index < numCourses; index++){
+        for(int index = 0; index < visited.length; index++){
             if(visited[index] != 1){
-                if(cycle(map, visited, index)){
+                if(cycle(map, visited, index))
                     return false;
-                }
             }
         }
         return true;
     }
-    private boolean cycle(ArrayList<ArrayList<Integer>> map, int[] visited, int index){
+    public boolean cycle(ArrayList<ArrayList<Integer>> map, int[] visited, int index){
         if(visited[index] != 0) return visited[index] == -1;
         visited[index] = -1;
         ArrayList<Integer> must = map.get(index);
